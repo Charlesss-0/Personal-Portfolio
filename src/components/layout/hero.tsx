dad394103ef1b2bc4953839b2905df244c63c743
Charless404 <aragonaugusto588@gmail.com>
2024-02-29 22:24:15 -0600

    switch typewriter effect to subheading

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -1,13 +1,13 @@
 import Particles from '../animation/particles'
-import Typewriter from './heading'
+import Typewriter from '../ui/typewriter'
 
 export default function Hero() {
 	return (
 		<div className="h-screen w-full">
 			<Particles />
 			<div className="absolute top-[50%] right-[5%] translate-y-[-50%] text-white londrina">
+				<p className="text-[8rem] font-bold">Carlos Aragon</p>
 				<Typewriter></Typewriter>
-				<p className="text-[3rem] font-bold">Front-end Web Developer</p>
 			</div>
 		</div>
 	)
--- src/components/ui/typewriter.tsx
+++ src/components/ui/typewriter.tsx
@@ -3,7 +3,7 @@ import { animate, motion, useMotionValue, useTransform } from 'framer-motion'
 import { useEffect } from 'react'
 
 export default function Typewriter() {
-	const baseText = 'Carlos Aragon' as string
+	const baseText = 'Front-end Web Developer'
 	const count = useMotionValue(0)
 	const rounded = useTransform(count, latest => Math.round(latest))
 	const displayText = useTransform(rounded, latest => baseText.slice(0, latest))
@@ -14,7 +14,7 @@ export default function Typewriter() {
 			duration: 1,
 			ease: 'easeInOut',
 			repeat: Infinity,
-			repeatDelay: 3,
+			repeatDelay: 5,
 			repeatType: 'reverse',
 		})
 
@@ -23,7 +23,7 @@ export default function Typewriter() {
 
 	return (
 		<span>
-			<motion.span className="text-[8rem] font-bold">{displayText}</motion.span>
+			<motion.span className="text-[3rem] font-bold">{displayText}</motion.span>
 			<CursorBlinker />
 		</span>
 	)
@@ -47,7 +47,7 @@ function CursorBlinker() {
 		<motion.div
 			variants={cursorVariant}
 			animate="blinking"
-			className="inline-block h-[5rem] w-[5px] rounded-sm translate-y-1 bg-[#0B60B0]"
+			className="inline-block h-[3rem] w-[5px] rounded-sm translate-y-1 bg-[#0B60B0]"
 		/>
 	)
 }
