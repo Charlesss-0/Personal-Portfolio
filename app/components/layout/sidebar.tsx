e0cfdb70908ae34990ab416a0b281062e8bbe33e
Charless404 <aragonaugusto588@gmail.com>
2024-03-17 19:30:02 -0600

    add hover effect on sidebar links

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -14,19 +14,55 @@ const Container = styled.div`
 	color: #fff;
 `
 
+const LinkItem = styled.li`
+	position: relative;
+	width: 6rem;
+	display: flex;
+	justify-content: center;
+	overflow: hidden;
+
+	&::after {
+		content: '';
+		position: absolute;
+		top: 50%;
+		left: 0;
+		width: 100%;
+		height: 5px;
+		border-radius: 50rem;
+		background: #008dda;
+		transform: translate(-100%, -50%);
+		transition: all 400ms;
+	}
+
+	&:hover::after {
+		transform: translateX(0%);
+	}
+`
+
 export default function Sidebar() {
+	const items = [
+		{
+			name: 'Contact',
+			link: '',
+		},
+		{
+			name: 'Details',
+			link: '',
+		},
+		{
+			name: 'Projects',
+			link: '#projects',
+		},
+	]
+
 	return (
 		<Container>
 			<ul className="flex justify-around w-full text-[1.2rem] [&>li>a]:cursor-pointer">
-				<li>
-					<a>Contact</a>
-				</li>
-				<li>
-					<a>Details</a>
-				</li>
-				<li>
-					<a href="#projects">Projects</a>
-				</li>
+				{items.map(item => (
+					<LinkItem key={item.name}>
+						<a href={item.link}>{item.name}</a>
+					</LinkItem>
+				))}
 			</ul>
 		</Container>
 	)
