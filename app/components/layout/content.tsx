38d0fffebe999aa62670a678c890f9eeb26d4f27
Charless404 <aragonaugusto588@gmail.com>
2024-03-17 19:08:04 -0600

    update view content button style

--- app/components/layout/content.tsx
+++ app/components/layout/content.tsx
@@ -6,49 +6,33 @@ import etchASketch from '../../assets/images/etchAsketch.webp'
 import styled from 'styled-components'
 
 const ViewProjects = styled.button`
-	border: 1px solid #efefef;
+	font-size: 1.5rem;
 	padding: 0.5rem 1rem;
+	transition: all 0.2s ease-in;
 	position: relative;
+	z-index: 1;
 	overflow: hidden;
-	transition: all 0.1s ease-in;
-	border: none;
-
-	&::before {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background: #fff5;
-		transition: all 500ms;
-		transform: translateX(-50%);
-		z-index: -1;
-	}
-
-	&::after {
-		content: '';
-		position: absolute;
-		top: 0;
-		right: 0;
-		width: 100%;
-		height: 100%;
-		background: #fff5;
-		transition: all 500ms;
-		transform: translateX(50%);
-		z-index: -1;
-	}
-
-	&:hover::before {
-		transform: translateX(-100%);
-	}
-
-	&:hover::after {
-		transform: translateX(101%);
-	}
+	border: 1px solid #fff;
 
 	&:hover {
-		outline: 1px solid #efefef;
+		color: black;
+	}
+
+	&:after {
+		content: '';
+		background: #fff;
+		position: absolute;
+		z-index: -1;
+		left: -20%;
+		right: -20%;
+		top: 0;
+		bottom: 0;
+		transform: skewX(-45deg) scale(0, 1);
+		transition: all 0.5s;
+	}
+
+	&:hover:after {
+		transform: skewX(-45deg) scale(1, 1);
 	}
 
 	&:active {
@@ -81,7 +65,7 @@ export default function Content(): React.ReactNode {
 	]
 
 	return (
-		<div id="projects" className="p-[2rem]">
+		<div id="projects" className="p-[2rem] ">
 			<ul className="flex flex-col gap-[25rem]">
 				{projects.map((item, index) => (
 					<li
@@ -94,9 +78,9 @@ export default function Content(): React.ReactNode {
 							<Card img={item.img} alt={item.name} />
 						</div>
 
-						<div className="w-full text-center p-[1rem] flex flex-col gap-[2rem] justify-center items-center">
-							<h1 className="text-[1.5rem]">{item.name}</h1>
-							<p className="w-[50%] text-[1.1rem]">{item.description}</p>
+						<div className="w-full text-center p-[1rem] flex flex-col gap-[2.5rem] justify-center items-center">
+							<h1 className="text-[2rem]">{item.name}</h1>
+							<p className="w-[50%] text-[1.2rem]">{item.description}</p>
 							<a href={item.url} target="_blank">
 								<ViewProjects>
 									<span>View Project</span>
--- app/components/layout/hero.tsx
+++ app/components/layout/hero.tsx
@@ -3,7 +3,7 @@ import Typewriter from '../ui/typewriter'
 
 export default function Hero() {
 	return (
-		<div className="h-screen w-full">
+		<div className="h-screen w-full bg-black">
 			<ParticlesBg />
 			<div className="absolute top-[50%] right-[5%] translate-y-[-50%] text-white select-none londrina">
 				<h1 className="text-[8rem] font-semibold md:text-[3rem]">
